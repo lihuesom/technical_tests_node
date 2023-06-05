975333055bc4ff82ffb2e466e7e5cd921cf94a83
lihuesom <linahueso03@gmail.com>
2023-06-04 19:48:26 -0500

    importando middlewares y agregando log por si las

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const routerApi = require('./routes');
+const {logErrors, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler );
+
 
 app.listen(port, () =>{
   console.log("port: " + port);
--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -8,6 +8,7 @@
  * @param {function} next - Función para pasar al siguiente middleware.
  */
 function logErrors(err, req, res, next) {
+  console.log('logErrors');
   console.error(err);
   next(err);
 }
@@ -22,6 +23,7 @@ function logErrors(err, req, res, next) {
  * @param {function} next - Función para pasar al siguiente middleware.
  */
 function errorHandler(err, req, res, next) {
+  console.log('errorHandler');
   res.status(500).json({
     message: err.message,
     stack: err.stack, // mostrar información del error
